a6db2cd7b8d62f0788bdf66af93f31a032d91a1c
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-24 10:52:38 +0000

    commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+//we get error because javascript engine is hidden in browser and when we use console, it is in the form of a document.
+
+console.log(3+3); console.log("Ria");
+//not readable
+
+/*
+Documentation - tc39, mdn
+*/
+
+let ame = "ria"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value, representation of empty value
+//undefined => var declared but value not assigned
+
+//symbol => unique
+
+//object 
+
+console.log(typeof "ria");
+
+console.log(typeof null);      //object
+
+console.log(typeof undefined); //undefined
+
+
+
