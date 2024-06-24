6d1a2048fca0425c1f75172bbef51797d3d52ea3
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-24 11:11:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score = "33abc"    NAN
+//let score = null       0
+//let score = undefined  NAN    
+//let score = true         1
+//let score = "Ria"       NAN since it is not convertible
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);   //NAN
+
+
+// "33" => 33
+// "33abc" => NaN
+
+
+//let isLoggedIn = 1          => true
+//let isLoggedIn = 0          => false
+//let isLoggedIn = ""         => false
+//let isLoggedIn = "ria"      => true
+
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);   //string
