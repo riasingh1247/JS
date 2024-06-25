2a7c31e93a2608363904b82089557e203635c00d
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-25 16:07:45 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,60 @@
+// Primiive Data Type
+// 7 types
+//call by value
+
+//String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null   //datatype => object
+let userEmail;  //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+const bigNumber = 34565736575683n
+
+//statically typed or dynamically typed
+
+
+
+
+
+// Non Primitive or Reference Types
+//Array, Objects, Functions
+
+const heros = ["Sid", "Vd" , "Shahid"]
+
+let myObj = {
+    name: "ria", 
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber); //undefined
+
+console.log(typeof myFunction); //function
+
+console.log(typeof heros); //object
+
+
+/*
+Primitive datatypes:
+String - string
+Number - number
+Boolean - Boolean
+Null - object(****)
+Undefined - undefined
+BigInt - bigint
+
+Non-primitive datatypes:
+Array - object
+Function - function(object)
+Object - object
+*/
