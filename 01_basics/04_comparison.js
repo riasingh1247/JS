547719f8276788ef75f43637f8c1aff273d2c73e
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-25 15:52:36 +0000

    comparison operations

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 = 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1); //automatically converted to number
+
+// console.log(null > 0); //f
+// console.log(null == 0); //f
+// console.log(null >= 0); //t
+// //comparison converts null to a number or 0
+
+// console.log(undefined==0); //f
+// console.log(undefined==0); //f
+// console.log(undefined==0); //f
+
+//comparison and equality check work differently
+
+// === => strict check, check the datatype
+
+console.log("2" === 2);
+
