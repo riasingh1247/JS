5a77ebb21acb63583c55350499c3d78ccf4ca72a
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-27 16:33:36 +0000

    nums

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString()); //3 100 has 3 digits
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966 //a bigger number is given as exponential
+console.log(otherNumber.toPrecision(3)) //returns a string
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); //values with commas
+
+/*********************MATHS************************************************ */
+
+
+
+
+
