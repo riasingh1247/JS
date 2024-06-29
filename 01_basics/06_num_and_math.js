83ffa50257f9409f5f2dcfd60f5e2a62946c9cc9
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-06-29 12:25:26 +0000

    math and num

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -15,6 +15,23 @@ console.log(hundreds.toLocaleString('en-IN')); //values with commas
 
 /*********************MATHS************************************************ */
 
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.2)) //upper value
+console.log(Math.floor(4.2)) //lower value
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+console.log(Math.random()); //random values b/w 0 and 1
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max-min +1)) + min)
+
+
+
 
 
 
