d3879ccf829c9831d8bd242323575526f4c16ed2
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-07-05 12:56:35 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,45 +1,78 @@
-// const facebookUser = new Object()  -> singleton
-const facebookUser = {}   //both have same o/p but 1st is singleton
+// // const facebookUser = new Object()  -> singleton
+// const facebookUser = {}   //both have same o/p but 1st is singleton
 
-facebookUser.id = "123abc"
-facebookUser.name = "Ria"
-facebookUser.isLoggedIn = false;
+// facebookUser.id = "123abc"
+// facebookUser.name = "Ria"
+// facebookUser.isLoggedIn = false;
 
+// // console.log(facebookUser);
+
+// const regularUser = {
+//     email: "ria@gmail.com",
+//     fullname : {
+//         userfullname: {
+//             firstname: "ria",
+//             lastname:  "singh"
+//         }
+//     }
+// }
+
+// // console.log(regularUser.fullname?.userfullname.firstname); // ? is used for if else condition
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+
+// // const obj3 = {obj1, obj2}
+// // const obj3 = Object.assign({}, obj1, obj2) // {} is an optional parameter
+
+// // const obj3 = {...obj1, ...obj2}
+// // //console.log(obj3);
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "riasingh@gmail.com"
+//     }
+// ]
+
+// // users[1].email
 // console.log(facebookUser);
 
-const regularUser = {
-    email: "ria@gmail.com",
-    fullname : {
-        userfullname: {
-            firstname: "ria",
-            lastname:  "singh"
-        }
-    }
+// console.log(Object.keys(facebookUser)); //datatype -> array
+// console.log(Object.values(facebookUser));
+// console.log(Object.entries(facebookUser))
+
+// console.log(facebookUser.hasOwnProperty('isLoggedIn')); //true
+
+const course = {
+    courname: "JS",
+    price: "1000",
+    courseInstructor: "Ria Singh"
 }
 
-// console.log(regularUser.fullname?.userfullname.firstname); // ? is used for if else condition
+// const {courseInstructor} = course
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
+const {courseInstructor: instructor} = course //de-structure object
 
-// const obj3 = {obj1, obj2}
-// const obj3 = Object.assign({}, obj1, obj2) // {} is an optional parameter
+// console.log(courseInstructor);
+console.log(instructor);
 
-// const obj3 = {...obj1, ...obj2}
-// //console.log(obj3);
+// const navbar = ({company}) => { //destructuring
 
-const users = [
-    {
-        id: 1,
-        email: "riasingh@gmail.com"
-    }
-]
+// }
 
-// users[1].email
-console.log(facebookUser);
+// navbar(company = "Ria")
 
-console.log(Object.keys(facebookUser)); //datatype -> array
-console.log(Object.values(facebookUser));
-console.log(Object.entries(facebookUser))
+/*
+{
+    "name": "Ria Singh",
+    "coursename": "JS",
+    "price": "free"
+}
+    */
 
-console.log(facebookUser.hasOwnProperty('isLoggedIn')); //true
+[
+    {},
+    {},
+    {}
+] //API can also be an array => JSON Format
