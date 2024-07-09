cfdbec1871f707f288058480b052ad8999ba8f57
Ria Singh <125139195+riasingh1247@users.noreply.github.com>
2024-07-09 14:57:12 +0000

    control flow

---
+++ 04_controlflow.js/one.js
@@ -0,0 +1,20 @@
+// //if
+
+// const isUserLoggedIn = true
+// const temp = 41
+
+// if(temp < 50){
+//     console.log("Less than 50");
+// }
+// console.log("Temperature is greater than 50");
+
+// Comparison Operator  < , > , <=, >=, == (operator assignment), !=, === (datatype)l
+
+
+// const isUserLoggedIn = true
+// const debitCard = true
+
+// is (userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
---
+++ 04_controlflow.js/truthy.js
@@ -0,0 +1,49 @@
+// /*
+// const userEmail = "ria@gmail.com"
+
+// if(userEmail){
+//     console.log("Got user email");
+// }
+
+// else{
+//     console.log("Dont have user email");
+// } //Got user email
+
+
+// const userEmail = {}
+
+// if(userEmail){
+//     console.log("Got user email");
+// }
+
+// else{
+//     console.log("Dont have user email");
+// } //Got user email
+
+// const userEmail = []
+
+// if(userEmail){
+//     console.log("Got user email");
+// }
+
+// else{
+//     console.log("Dont have user email");
+// }//Got user email
+
+// */
+
+
+// // falsy values - false, 0, -0, BigInt 0n, null, undefined, NaN, ""
+
+// //truthy values - "0", 'false', " ", [], {}, function(){}
+
+// // Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+
+// console.log(val1);
+
+// //Ternary Operator => condition ? true: false
+
